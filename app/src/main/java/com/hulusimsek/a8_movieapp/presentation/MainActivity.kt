30cebdb882ea1819ef230ac41e337ca89a790e42
Hulusi Şimşek <hulusimsek@gmail.com>
2024-08-21 02:34:21 +0300

    Build gradle edited
    Libraries added

---
+++ app/src/main/java/com/hulusimsek/a8_movieapp/presentation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.hulusimsek.a8_movieapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.hulusimsek.a8_movieapp.presentation.theme.ui._8_MovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _8_MovieAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
